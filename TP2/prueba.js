e0b0482ed0ee90e28b747b793957732b01eab462
DamianBlum <62249397+DamianBlum@users.noreply.github.com>
2021-04-04 22:29:53 -0300

    Arranque TP2

---
+++ TP2/prueba.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'secret',
+});
+ 
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+ 
+/*connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();*/
