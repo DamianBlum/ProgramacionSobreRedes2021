d3655405e202b1fb466019318030ac5d62eaf8c6
DamianBlum <62249397+DamianBlum@users.noreply.github.com>
2021-06-06 22:33:53 -0300

    Tests hechos

--- TP3/Pruebas/testsFunciones.js
+++ TP3/Pruebas/testsFunciones.js
@@ -2,29 +2,16 @@ const axios = require("axios").default;
 
 console.log("TEST");
 console.log("");
-console.log("Reserva: 6 butacas * 6 users * 4 = 144 peticiones");
+console.log("Funciones: 6 users *4 = 144 peticiones");
 
 let promesas = [];
 let start = Date.now();
-let arrayButacas = ["a3", "b1", "b2", "b3", "b4", "c2"];
 for (let k = 0; k < 4; k++) {
   for (let i = 1; i <= 6; i++) {
-    for (let j = 0; j < arrayButacas.length; j++) {
-      const butaca = arrayButacas[j];
       promesas.push(
-        axios.post("http://localhost:3304/4/reservar", {
-          user_id: i,
-          butacas: `["${butaca}"]`,
-        })
-        /*
-        axios.get("http://localhost:3304/funciones", {
-          user_id: i,
-          butacas: `["${butaca}"]`,
-        })
-        */
+        axios.get("http://localhost:3304/funciones")
       );
     }
-  }
 }
 
 Promise.all(promesas).then((res) => {
@@ -40,15 +27,10 @@ start2 = Date.now();
 async function prueba() {
 for (let k = 0; k < 4; k++) {
   for (let i = 1; i <= 6; i++) {
-    for (let j = 0; j < arrayButacas.length; j++) {
-      const butaca = arrayButacas[j];
       await promesas2.push(
-        await axios.post("http://localhost:3305/4/reservar", {
-          user_id: i,
-          butacas: `["${butaca}"]`,
-        })
+        await axios.get("http://localhost:3305/funciones")
       );
-    }}
+    }
   }
   let end2 = Date.now();
   console.log("");
